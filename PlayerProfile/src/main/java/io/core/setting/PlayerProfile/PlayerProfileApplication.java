d62cfd8863b8e5701dfcaa3376db272145756a75
rvsales <rvsales@hotmail.com>
2018-10-12 22:59:45 -0300

    new changes

--- PlayerProfile/src/main/java/io/core/setting/PlayerProfile/PlayerProfileApplication.java
+++ PlayerProfile/src/main/java/io/core/setting/PlayerProfile/PlayerProfileApplication.java
@@ -3,7 +3,6 @@ package io.core.setting.PlayerProfile;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class PlayerProfileApplication {
 
